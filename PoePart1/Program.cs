9f39270b3c06f33897a47898c4f5694a1c29ae37
Yolanda Mashaba <nkokayolanda@gmail.com>
2025-03-27 11:22:20 +0200

    feat: implement CyberChat integration layer and enhance UX -Add CyberChat class as central coordinator for: *Seamless integration of ResponseSystem, VoiceGreeting and AsciiArt *User onboarding with welcome messages *Unified UI/UX flow management -Refactor Main class to serve as entry point for chatbot execution -Enhance user exoerience with: *Consistent interaction formatting *Error handling wrapper *Visual feedback improvements *Colorful text to make the interaction with the chatot fun and inviting *Divider to make the interaction with the chatbot easy

--- PoePart1/Program.cs
+++ PoePart1/Program.cs
@@ -13,10 +13,17 @@ namespace PoePart1
             new VoiceGreeting() { };
             new AsciiArt() { };
             new ResponseSystem() { };
+            new CyberChat() { };
 
             //Define file paths
            string audioFilePath = "C:\\Users\\ntsal\\source\\repos\\PoePart1\\resources\\greeting.wav";//Path to the audio recording
            string responseFilePath = "C:\\Users\\ntsal\\source\\repos\\PoePart1\\resources\\responses.txt";//Path to the responses file
+
+            //Instantiate the chatbot 
+            CyberChat chat = new CyberChat(responseFilePath, audioFilePath);
+
+            //Run the chat
+            chat.Run();
         }
     }
 }
